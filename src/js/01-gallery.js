b3a0ea5d97cd3cf523616abe5c04ecb5fd3ca86b
Yevhenii Sitolenko <110056243+Yevhenii770@users.noreply.github.com>
2023-08-19 15:26:14 +0200

    don't now, just upd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,23 +3,56 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+
 const galleryDiv = document.querySelector('.gallery');
 const gallaryMarkup = createGalaryMarkup(galleryItems);
+
+galleryDiv.addEventListener('click', onGallaryClick);
+galleryDiv.insertAdjacentHTML('beforeend', gallaryMarkup);
 
 
 function createGalaryMarkup(galleryItems) {
     return galleryItems.map(({ preview, original, description }) => {
-        return `<a class="gallery__item" href="${original}">
-  <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>`
+        return `<div class="gallery__item">
+        <a class="gallery__link" href="${preview}">
+        <img
+      class="gallery__image"
+      src="${original}"
+      data-source="${original}"
+      alt="${description}"
+    />
+    </a>
+    </div>`
     }).join('')
 };
 
-galleryDiv.insertAdjacentHTML('beforeend', gallaryMarkup);
 
+function onGallaryClick(event) {
+  event.preventDefault();
+  
+  const isGallarySwatchEl = event.target.classList.contains('gallery__image');
+  if (!isGallarySwatchEl) {
+    return;
+  };
 
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+  `);
+  
 
-let gallery = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: '250'});
-gallery.on('show.simplelightbox', function () {
-	// Do something…
-});
+  instance.show();
+  
+  function onEscClose(event) {
+    if (event.key === 'Escape') {
+      instance.close();
+    };
+};
+
+  if (instance.visible()) {
+    window.addEventListener('keydown', onEscClose);
+  } if (!instance.visible()) {
+    window.removeEventListener('keydown', onEscClose);
+  }
+
+}
